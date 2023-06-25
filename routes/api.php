d5870ee53104b2bd254f249cdf0282c6e0b280e3
Bitsapascal <bitapascal033@gmail.com>
2023-06-26 00:01:25 +0100

    on a retravaillé l'ajout et le rerait de la quantité, l'ajout et le retrait d'un animal en promotion

--- routes/api.php
+++ routes/api.php
@@ -21,27 +21,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 // Routes protégées
-Route::group(['middleware'=>'auth:sanctum'],function(){
-    // User
-    Route::put('users/update/{id}',[AuthController::class,'update']);
-    Route::delete('users/delete/{id}',[AuthController::class,'delete']);
-    Route::post('users/logout',[AuthController::class,'logout']);
-    // Animal
-    Route::post('animals/store', [AnimalController::class,'store']);
-    Route::put('animals/update/{id}', [AnimalController::class,'update']);
-    Route::delete('animals/delete/{id}', [AnimalController::class,'delete']);
-    Route::put('animals/update/promotion/{id}', [AnimalController::class,'promotion']);
-    Route::put('animals/update/qte/in/{id}', [AnimalController::class,'addQte']);
-    Route::put('animals/update/qte/out/{id}', [AnimalController::class,'removeQte']);
+// Route::group(['middleware'=>'auth:sanctum'],function(){
+//     // User
     
-});
+//     // Animal
+    
+// });
 // Routes publiques
 // User
 Route::post('users/store',[AuthController::class,'store']);
 Route::post('users/login',[AuthController::class,'login']);
 Route::get('users/get',[AuthController::class,'getAll']);
 Route::get('users/get/{id}',[AuthController::class,'getOne']);
+
+Route::put('users/update/{id}',[AuthController::class,'update']);
+Route::delete('users/delete/{id}',[AuthController::class,'delete']);
+Route::post('users/logout',[AuthController::class,'logout']);
+
 // Animal
+
+Route::post('animals/store', [AnimalController::class,'store']);
+Route::put('animals/update/{id}', [AnimalController::class,'update']);
+Route::delete('animals/delete/{id}', [AnimalController::class,'delete']);
+Route::put('animals/update/promotion/{id}', [AnimalController::class,'promotion']);
+Route::put('animals/update/qte/in/{id}', [AnimalController::class,'addQte']);
+Route::put('animals/update/qte/out/{id}', [AnimalController::class,'removeQte']);
+
 Route::get('animals/get', [AnimalController::class,'getAll']);
 Route::get('animals/get/{id}', [AnimalController::class,'getOne']);
 Route::get('animals/get/byName/{name}', [AnimalController::class,'getByName']);
